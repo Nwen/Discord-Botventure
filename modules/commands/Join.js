e3e2b1a4ae08f8e09a84fc94f1836545bb881595
Neltab <aurelienboissiere@gmail.com>
2020-02-23 10:35:22 +0100

    Ajout de diverses commandes

--- modules/commands/Join.js
+++
@@ -1,7 +0,0 @@
-const PlayerManager = require("../PlayerManager");
-
-const JoinCommand = async function(message, args){
-    PlayerManager.addNewPlayer(message, message.author.id, args[1]);
-}
-
-module.exports.JoinCommand = JoinCommand;
