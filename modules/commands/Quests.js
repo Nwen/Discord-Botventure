739274a3a99e1a9ea935d6c648f0ad9ced79fc45
Neltab <aurelienboissiere@gmail.com>
2020-02-29 17:26:43 +0100

    Test suppression du message

--- modules/commands/Quests.js
+++ modules/commands/Quests.js
@@ -2,50 +2,57 @@ const Discord = require("discord.js");
 const PlayerManager = require("../PlayerManager");
 let embed;
 let emojis = ["1️⃣","2️⃣","3️⃣"];
+let player = PlayerManager.getPlayerByID(message.author.id);
 
 async function questCommand(message){
-    embed = new Discord.RichEmbed();
-    embed.setTitle("**Panneau d'affichage des quêtes**");
-    embed.setDescription("Vous retrouverez ici toutes les quêtes disponibles.\nPour séléctionner une quête, cliquez sur la réaction correspondante.")
-    embed.addField(":one: Quete 1","General description about a guy who has lost his favorite chicken",false);
-    embed.addField(":two: Quete 2","General description about a guy who has lost his favorite chicken",false);
-    embed.addField(":three: Quete 3","General description about a guy who has lost his favorite chicken",false);
+    if(player != null){
+        embed = new Discord.RichEmbed();
 
-    let reponse = await displayReac(message);
-    let questIsTaken = false;
+        let reponse = await displayReac(message);
+        let questIsTaken = false;
 
-    let player = PlayerManager.getPlayerByID(message.author.id);
+        const filter = (reaction, user) => {
+            return (reactionIsCorrect(reaction) && user.id === message.author.id);
+         };
+         const collector = reponse.createReactionCollector(filter, {
+            time: 120000
+         });
 
-    const filter = (reaction, user) => {
-        return (reactionIsCorrect(reaction) && user.id === message.author.id);
-     };
-     const collector = reponse.createReactionCollector(filter, {
-        time: 120000
-     });
+         collector.on('collect', (reaction) => {
+                if(!questIsTaken && !player.getOccupationState()){
+                    message.channel.send(`Vous avez choisis la quete ${reaction.emoji.name}`);
+                    questIsTaken = true;
+                    player.setOccupationState(true); // Player is occupied
+                }else {
+                    message.channel.send(`Vous avez déjà une quete en cours`);
+                }
+         });
 
-     collector.on('collect', (reaction) => {
-        if(player != null){
-            if(!questIsTaken && !player.getOccupationState()){
-                message.channel.send(`Vous avez choisis la quete ${reaction.emoji.name}`);
-                questIsTaken = true;
-                player.setOccupationState(true); // Player is occupied
-            }else {
-                message.channel.send(`Vous avez déjà une quete en cours`);
-            }
-        }else{
-            message.channel.send(`Il semblerait que vous n'avez pas encore créé votre personnage`);
-        }
-     });
+         collector.on('end', () =>{
+            reponse.delete();
+         });
+    }else{
+        message.channel.send(`Il semblerait que vous n'avez pas encore créé votre personnage`);
+    }
+
 }
 
 const displayReac = function(message){
-    return message.channel.send(embed).then(async msg =>{
-
-        for(i in emojis){
-            await msg.react(emojis[i]);
-        }
-        return msg;
-    });
+    if(!player.getOccupationState()){
+        embed.setTitle("**Panneau d'affichage des quêtes**");
+        embed.setDescription("Vous retrouverez ici toutes les quêtes disponibles.\nPour séléctionner une quête, cliquez sur la réaction correspondante.")
+        embed.addField(":one: Quete 1","General description about a guy who has lost his favorite chicken",false);
+        embed.addField(":two: Quete 2","General description about a guy who has lost his favorite chicken",false);
+        embed.addField(":three: Quete 3","General description about a guy who has lost his favorite chicken",false);
+        return message.channel.send(embed).then(async msg =>{
+    
+            for(i in emojis){
+                await msg.react(emojis[i]);
+            }
+            return msg;
+        });
+    }
+    
 };
 
 const reactionIsCorrect = function (reaction) {
